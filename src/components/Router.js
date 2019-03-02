236535873d9e693b4c60d98eec74c071ed9b5261
Darko Radovic <darmio5@gmail.com>
2019-03-02 16:56:06 +0100

    router

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 
 import App from '../App';
 import Recipe from './Recipe';
 
 const Router = () => {
   return(
-  <BrowserRouter>
+  
     <Switch>
       <Route path="/" component={App} exact />
       <Route path="/recipe/:id" component={Recipe} />
     </Switch>
-  </BrowserRouter>
+  
   )
 }
 
